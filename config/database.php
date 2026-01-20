d29973a6c96008e9d6bbcfd5eb37f2d7f7118abf
okore-tech <joysuzie104@gmail.com>
2026-01-20 21:42:38 +0300

    moved the db.sqlite to a folder named database

--- config/database.php
+++ config/database.php
@@ -1,8 +1,15 @@
 <?php
 try {
-    $dbPath = __DIR__ . "/db.sqlite";
-    $conn = new PDO("sqlite:" . $dbPath);
+    $db_path = __DIR__ . "/../database/db.sqlite";
+
+    if (!file_exists($db_path)) {
+        die("Database file not found at $db_path");
+    }
+
+    $conn = new PDO("sqlite:" . $db_path);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
+
+} catch(PDOException $e) {
     die("Database connection failed: " . $e->getMessage());
 }
+?>
